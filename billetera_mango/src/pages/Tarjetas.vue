4aadf6b34e52026492e19a5a4a9ce86be8e6f6f2
Lola Diaz-Varela <ldiazvarela@itba.edu.ar>
2025-04-30 21:34:37 -0300

    Apretar afuera del pop up lo cierra

--- billetera_mango/src/pages/Tarjetas.vue
+++ billetera_mango/src/pages/Tarjetas.vue
@@ -63,7 +63,7 @@
     </v-row>
 
     <!-- Dialogo para agregar nueva tarjeta -->
-    <v-dialog v-model="dialog" persistent max-width="600px">
+    <v-dialog v-model="dialog" max-width="600px">
       <v-card>
         <v-card-title class="d-flex align-center">
           <!-- --- Back Arrow Button --- -->
